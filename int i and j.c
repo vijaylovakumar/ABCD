0b5231f42253d67ac22e101142773856f33f2b16
Vijaya KUMAR <vijaya.kumar@randstaddigital.com>
2024-01-05 08:44:05 +0530

    commiting all files practice

---
+++ int i and j.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+    // declaration i and j.
+    int i;
+    int j;
+
+   for (j=0;j<=10;j++)
+   {
+
+        for(i=0 ;i<=5 ;i++ )
+        {
+            printf(" i = %d.\n",i);
+        }
+
+   printf("j=%d.\n",j);
+   }
+   return 0;
+}
---
+++ sampleloop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    // declaration i.
+    int i;
+
+   for(i=0 ;i<=5 ;i++ ) {
+      printf(" i = %d.\n",i);
+   }
+
+   return 0;
+}
